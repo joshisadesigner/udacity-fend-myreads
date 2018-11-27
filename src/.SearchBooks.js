2ec417277a42ffc36e98877fd226ab89e526998c
Josh <joshisadesigner@gmail.com>
2018-11-27 11:17:26 -0600

    Add SearchBooks component

---
+++ src/.SearchBooks.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class SearchBooks extends Component {
+    render() {
+        return <p>this is the search!</p>;
+    }
+}
+
+export default CreateContact;
